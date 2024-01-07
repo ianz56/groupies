36b757f174b883f38dcb1bfbbf71eec6a3740869
ianz56 <ianperdiansah05@gmail.com>
2024-01-07 23:21:28 +0700

    Login new

--- login.php
+++ login.php
@@ -14,7 +14,7 @@ if (isset($_SESSION['username'])) {
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>GroupiesSaver - Login</title>
-  <link rel="stylesheet" href="css/style.css?v=2" />
+  <link rel="stylesheet" href="/css/style.css?v=1.2" />
   <script src="https://kit.fontawesome.com/d1c5590ed7.js" crossorigin="anonymous"></script>
   <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png" />
@@ -59,8 +59,6 @@ if (isset($_SESSION['username'])) {
         </label>
 
         <div id="loading" style="display: none;">Loading...</div>
-
-        <!-- Ganti input type dari "button" menjadi "submit" -->
         <input type="submit" class="btn btn-success btn-sm btn-block login" value="Login" hidden />
         <div class="submit-button" onclick="loginAjax()">Login</div>
       </form>
